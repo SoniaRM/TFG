d05171fa2e2b249915e322c0d9680a819470f2e6
SoniaRM <sonrusmor@alum.us.es>
2025-02-25 11:54:34 +0100

    preparación proyecto

--- tfgapp/urls.py
+++ tfgapp/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tfgapp import views
+from app import views
 
 urlpatterns = [
-    path('hola', views.index),
     path('admin/', admin.site.urls),
 ]
